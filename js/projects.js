7f8edf53fc98d7d812ca437b4a369d1940f6e66d
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2026-02-18 22:25:05 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,26 +1,20 @@
-import('../js/project-data.js?v=' + new Date().getTime())
-        .then(module => {
-            const projects = module.projects;
+import { projects } from '../js/project-data.js';
 
-            const container = document.getElementById("project-container");
-            if (!container) return;
+const container = document.getElementById("project-container");
+if (container) {
+    projects.filter(project => project.staticThumbnail)
+    .forEach(project => {
+        const card = document.createElement("a");
+        card.classList.add("project-card");
 
-            projects.filter(project => project.staticThumbnail)
-            .forEach(project => {
-                const card = document.createElement("a");
-                card.classList.add("project-card");
-                
-                card.href = `project.html?title=${encodeURIComponent(project.title)}`;
-                card.innerHTML = `
-                    <img src="${project.staticThumbnail}" alt="${project.title}" />
-                    <p></p>
-                    <h>${project.title} (${project.year})</h>
-                    <p>${project.summary}</p>
-                `;
-
-                container.appendChild(card);
-            });
-
-        document.body.classList.add("ready");
-        })
-    .catch(error => console.error('Error loading projects:', error));
+        card.href = `project.html?title=${encodeURIComponent(project.title)}`;
+        card.innerHTML = `
+            <img src="${project.staticThumbnail}" alt="${project.title}" />
+            <p></p>
+            <h>${project.title} (${project.year})</h>
+            <p>${project.summary}</p>
+        `
+        container.appendChild(card);
+    });
+}
+document.body.classList.add("ready");
